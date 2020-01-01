13e20cfe0229949b5ede7816504ad3cbaf6c0fe1
Alexandre Seo <perso@alexandre-seo.tk>
2020-01-01 17:51:21 +0100

    Refactor to class

--- 42SlotSniper.user.js
+++ 42SlotSniper.user.js
@@ -9,7 +9,7 @@
 
 // ==UserScript==
 // @name     42 Slot Sniper
-// @version  1.0.4
+// @version  1.1.0
 // @include  https://projects.intra.42.fr/projects/*/slots*
 // @run-at   document-idle
 // @license  GPL-3.0-or-later
@@ -22,22 +22,77 @@
  * Add a style element to the document head and insert css into it
  * @param {string} css The CSS content
  */
-function addGlobalStyle(css) {
+(css => {
     let head, style;
     head = document.getElementsByTagName('head')[0];
     if (!head) { return; }
     style = document.createElement('style');
     style.type = 'text/css', style.innerHTML = css;
     head.appendChild(style);
-}
-addGlobalStyle('@keyframes pulse{0%{box-shadow:0 0 4px 1px #78c5d5}25%{box-shadow:0 0 4px 1px #79c268}50%{box-shadow:0 0 4px 1px #f5d63d}75%{box-shadow:0 0 4px 1px #e868a1}100%{box-shadow:0 0 4px 1px #bf63a6}}@keyframes pulse-bg{0%{background-color:#78c5d5}25%{background-color:#79c268}50%{background-color:#f5d63d}75%{background-color:#e868a1}100%{background-color:#bf63a6}}.is-loading{animation-name:pulse,pulse-bg;animation-duration:2s;animation-timing-function:ease-in-out;animation-direction:alternate;animation-iteration-count:infinite;color:white;border-style:hidden;}');
+})('@keyframes pulse{0%{box-shadow:0 0 4px 1px #78c5d5}25%{box-shadow:0 0 4px 1px #79c268}50%{box-shadow:0 0 4px 1px #f5d63d}75%{box-shadow:0 0 4px 1px #e868a1}100%{box-shadow:0 0 4px 1px #bf63a6}}@keyframes pulse-bg{0%{background-color:#78c5d5}25%{background-color:#79c268}50%{background-color:#f5d63d}75%{background-color:#e868a1}100%{background-color:#bf63a6}}.is-loading{animation-name:pulse,pulse-bg;animation-duration:2s;animation-timing-function:ease-in-out;animation-direction:alternate;animation-iteration-count:infinite;color:white;border-style:hidden;}');
 
-function stopSniping(button, interval) {
-	console.debug("SNIPING CANCELED !");
-	clearInterval(interval);
-	button.textContent = "SNIPER";
-	button.classList.remove("is-loading");
-	delete button.dataset.started;
+class Sniper {
+	constructor() {
+		this.team = window.location.search.split("=")[1];
+		this.DataDate = document.getElementsByClassName("fc-day-header");
+		this.project = /projects\/(.+)\//.exec(window.location.pathname)[1];
+		this.login = document.querySelector("span[data-login]").dataset.login;
+		this.button = document.createElement("button");
+		this.begin = this.DataDate[0].dataset.date;
+		this.end = this.DataDate[this.DataDate.length - 1].dataset.date;
+		this.period = 30000;
+
+		this.running = false;
+		this.button.textContent = "SNIPER";
+		this.button.addEventListener("click", () => {
+			if (this.running == false)
+				this.bind();
+			else
+				this.stop();
+		});
+
+		document.querySelector("div.fc-left").appendChild(this.button);
+	}
+
+	bind() {
+		this.interval = setInterval(this.snipe.bind(this), this.period);
+		this.snipe();
+		this.button.classList.add("is-loading");
+		this.button.textContent = "SNIPING";
+		this.running = true;
+	}
+
+	stop() {
+		console.debug("SNIPING STOPPED !");
+		clearInterval(this.interval);
+		this.button.textContent = "SNIPER";
+		this.button.classList.remove("is-loading");
+		this.running = false;
+	}
+
+	async snipe() {
+		let response = await fetch(
+			`https://projects.intra.42.fr/projects/${this.project}/slots.json?team_id=${this.team}&start=${this.begin}&end=${this.end}`, {
+			"credentials": "include",
+			"headers": {
+				"Accept": "application/json, text/javascript, */*; q=0.01",
+				"Accept-Language": "en-US,en;q=0.5",
+				"X-CSRF-Token": document.querySelector("meta[name='csrf-token']").content,
+				"X-Requested-With": "XMLHttpRequest",
+			},
+			"method": "GET",
+			"mode": "cors"
+		});
+		response = await response.json();
+
+		if (response.length) {
+			let message =	"Found slot for " + (new Date(response[0].start)).toLocaleString() +
+							"\nDo you want to take it ?";
+			if (window.confirm(message))
+				takeSlot(this.team, this.project, this.begin, this.end, response[0].ids.split(',')[0]);
+			this.stop()
+		}
+	}
 }
 
 async function takeSlot(team, project, begin, end, id) {
@@ -57,51 +112,7 @@ async function takeSlot(team, project, begin, end, id) {
 	});
 }
 
-async function checkForSlots(team, project, begin, end) {
-	let response = await fetch(
-		`https://projects.intra.42.fr/projects/${project}/slots.json?team_id=${team}&start=${begin}&end=${end}`, {
-		"credentials": "include",
-		"headers": {
-			"Accept": "application/json, text/javascript, */*; q=0.01",
-			"Accept-Language": "en-US,en;q=0.5",
-			"X-CSRF-Token": document.querySelector("meta[name='csrf-token']").content,
-			"X-Requested-With": "XMLHttpRequest",
-			"Cache-Control": "max-age=0"
-		},
-		"method": "GET",
-		"mode": "cors"
-	});
-	response = await response.json();
-
-	if (response.length) {
-		let message =	"Found slot for " + (new Date(response[0].start)).toLocaleString() +
-						"\nDo you want to take it ?";
-		if (window.confirm(message))
-			takeSlot(team, project, begin, end, response[0].ids.split(',')[0]);
-		clearInterval(checkForSlots.interval);
-		stopSniping(checkForSlots.button, clearInterval.interval)
-	}
-}
-
 (async () => {
-	let teamId = window.location.search.split("=")[1];
-	let DataDate = document.getElementsByClassName("fc-day-header");
-	let project = /projects\/(.+)\//.exec(window.location.pathname)[1];
-
-	let button = document.createElement("button");
-	checkForSlots.button = button;
-	button.textContent = "SNIPER";
-	button.addEventListener("click", () => {
-		if (!button.dataset.started) {
-			console.debug("SNIPING IN PROGRESS !");
-			checkForSlots.interval = setInterval(checkForSlots, 5000, teamId, project, DataDate[0].dataset.date, DataDate[DataDate.length - 1].dataset.date);
-			button.classList.add("is-loading");
-			button.textContent = "SNIPING";
-			button.dataset.started = "!";
-		} else
-			stopSniping(button, checkForSlots.interval);
-	});
-	let container = document.querySelector("div.fc-left");
-	container.appendChild(button);
+	let sniper = new Sniper;
 })();
 
